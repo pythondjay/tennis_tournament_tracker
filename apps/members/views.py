58251c33916f13415bec21e7dcbd19b9205ebbe4
Jaime <jay4000@hotmail.co.uk>
2023-09-06 14:59:32 +0200

    Started working on the members templates

--- apps/members/views.py
+++ apps/members/views.py
@@ -9,37 +9,29 @@ from .models import Members
 def home_page(request):
     member_cnt = Members.objects.all().count()
 
-    response = f"""
-    <html>
-    
-    <h1>Welcome to TennisTracker</h1>
-    
-    <p> {member_cnt} active users and counting </p>
-    
-    </html>
-    
-    """
+    context = {
+        "member_cnt": member_cnt,
+    }
 
-    return HttpResponse(response)
+    return render(
+        request,
+        "members/home.html",
+        context,
+    )
 
 
 def members_info(request, id):
     mem = Members.objects.get(pk=id)
 
-    response = f"""
-    <html>
-        <h1>Tennis Club Members</h1>
-        <p><b>First Name</b>: {mem.firstname}</p>
-        <p><b>Surname</b>: {mem.lastname}</p>
-        <p><b>Age</b>: {mem.age}</p>
-        <p><b>Email address</b>: {mem.email}</p>
-        <p><b>Phone Number</b>: {mem.phone}</p>
-        <p><b>Ability</b>: {mem.ability}</p>
-        <p><b>League</b>: {mem.league}</p>       
-    </html>
-    """
+    context = {
+        "mem": mem,
+    }
 
-    return HttpResponse(response)
+    return render(
+        request,
+        "members/members_info.html",
+        context,
+    )
 
 
 class HomePage(TemplateView):
